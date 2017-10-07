3573c26d9a977e9affb707870d2a045b9231fbd3
raymondmar61 <raym61@hotmail.com>
2017-10-07 02:11:14 -0700

    upload

--- 023nonabundantsums.py
+++ 023nonabundantsums.py
@@ -1,5 +1,5 @@
 #https://projecteuler.net/problem=23 Non-Abundant Sums
-#https://projecteuler.net/thread=22
+#https://projecteuler.net/thread=23
 #A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
 #A number n is called deficient if the sum of its proper divisors is less than n
 #A number n is called abundant if the sum of its proper divisors is greater than n.
@@ -8,29 +8,70 @@
 #Every integer greater than 20161 can be written as the sum of two abundant numbers https://en.wikipedia.org/wiki/Abundant_number
 #Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
 
-perfectnumberslist = []
-deficientnumberslist = []
 abundantnumberslist = []
+targetnumber = 20161
 number = 1
-while number <= 100:
+while number <= targetnumber:
 	divisorslist = []
-	for eachnumber in range(1,number-1):
+	for eachnumber in range(1,number):
 		if number % eachnumber == 0:
 			divisorslist.append(eachnumber)
-	#print(divisorslist)
 	sumdivisors = sum(divisorslist)
-	if sumdivisors == number:
-		#print("Perfect Number")
-		perfectnumberslist.append(number)
-	elif sumdivisors > number:
-		#print("Abundant sum divisors %d greater than number %d." %(sumdivisors, number))
+	if sumdivisors > number:
 		abundantnumberslist.append(number)
-	elif sumdivisors < number:
-		#print("Deficient sum divisors %d less than number %d." %(sumdivisors, number))
-		deficientnumberslist.append(number)
 	number += 1
-print("Perfect Numbers List",perfectnumberslist)
-print("Deficient Numbers List",deficientnumberslist)
-print("Abundant Numbers List",abundantnumberslist)
-#RM:  Stopped.  I can inefficiently calculate the perfect, abundant, and deficient numbers.  Not enough time to learn more math theory.  I move on solving more problems and learning more Python.
+abundantnumberslistsum = []
+for i in abundantnumberslist:
+	for j in abundantnumberslist:
+		ij = i + j
+		if ij > targetnumber:
+			pass
+		elif ij not in abundantnumberslistsum:
+			abundantnumberslistsum.append(ij)
+numberslist = list(range(1,targetnumber+1))
+for eachabundantnumberslistsum in abundantnumberslistsum:	
+	if (eachabundantnumberslistsum in numberslist):
+		numberslist.remove(eachabundantnumberslistsum)
+print(sum(numberslist)) #print 4179871
 
+# perfectnumberslist = []
+# deficientnumberslist = []
+# abundantnumberslist = []
+# number = 1
+# while number <= 100:
+# 	divisorslist = []
+# 	for eachnumber in range(1,number-1):
+# 		if number % eachnumber == 0:
+# 			divisorslist.append(eachnumber)
+# 	#print(divisorslist)
+# 	sumdivisors = sum(divisorslist)
+# 	if sumdivisors == number:
+# 		#print("Perfect Number")
+# 		perfectnumberslist.append(number)
+# 	elif sumdivisors > number:
+# 		#print("Abundant sum divisors %d greater than number %d." %(sumdivisors, number))
+# 		abundantnumberslist.append(number)
+# 	elif sumdivisors < number:
+# 		#print("Deficient sum divisors %d less than number %d." %(sumdivisors, number))
+# 		deficientnumberslist.append(number)
+# 	number += 1
+# #print("Perfect Numbers List",perfectnumberslist)
+# #print("Deficient Numbers List",deficientnumberslist)
+# print("Abundant Numbers List",abundantnumberslist)
+# abundantnumberslistsum = []
+# for i in abundantnumberslist:
+# 	for j in abundantnumberslist:
+# 		ij = i + j
+# 		if ij not in abundantnumberslistsum:
+# 			abundantnumberslistsum.append(ij)
+# abundantnumberslistsum.sort()
+# print(abundantnumberslistsum)
+# numberslist = []
+# for eachnumber in range(1,101):
+# 	numberslist.append(eachnumber)
+# print(numberslist)
+# for eachabundantnumberslistsum in abundantnumberslistsum:	
+# 	if (eachabundantnumberslistsum in abundantnumberslistsum) and (eachabundantnumberslistsum <= 100):
+# 		numberslist.remove(eachabundantnumberslistsum)
+# print(numberslist)
+# print(sum(numberslist))
